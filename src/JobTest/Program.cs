ab07340c1364f767524959dcd8c72320d1c657bb
Marko Žlogar <marko.zlogar@mikrocop.com>
2016-11-17 08:30:06 +0100

    Rename solution, project and namescapes

--- src/JobTest/Program.cs
+++ src/JobTest/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 
-namespace AureliaDemo
+namespace JobTest.Web
 {
     public class Program
     {
--- src/JobTest/Startup.cs
+++ src/JobTest/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace AureliaDemo
+namespace JobTest.Web
 {
     public class Startup
     {
